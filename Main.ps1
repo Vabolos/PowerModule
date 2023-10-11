e1cb1e6d6db6d7a63b2d801334d712b98c5474a4
Vabolos <vaboloslol@gmail.com>
2023-10-11 21:29:15 +0200

    Added a main script file to the repository

---
+++ Main.ps1
@@ -0,0 +1,7 @@
+# Variables to be used in the script, these are the only variables that should be changed
+
+# Path to the folder where the script is located
+$ScriptPath = "C:\Users\user\Desktop\Script"        # Change this to the path where the script is located
+
+# Path to the folder where the files are located
+$FilesPath = "C:\Users\user\Desktop\Files"          # Change this to the path where the files are located
