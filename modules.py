e3739a0ccd5c9162e8276decd95c99e35e4c0bcc
Vabolos <79908586+Vabolos@users.noreply.github.com>
2023-10-26 13:27:04 +0200

    added a new file containing module array for the GUI (cleaner code)

---
+++ modules.py
@@ -0,0 +1,29 @@
+# modules.py
+
+modules = [
+    "ActiveDirectory",
+    "AppLocker",
+    "Appx",
+    "AssignedAccess",
+    "BitLocker",
+    "BranchCache",
+    "Certificate",
+    "CimCmdlets",
+    "ConfigCI",
+    "ConfigDefender",
+    "ConfigManager",
+    "ConfigSPLA",
+    "ConfigTroubleshooting",
+    "DirectAccessClientComponents",
+    "Dism",
+    "DnsClient",
+    "DnsServer",
+    "EventTracingManagement",
+    "FailoverClusters",
+    "FileServer",
+    "GroupPolicy",
+    "Hyper-V",
+    "iSCSI",
+    "ISE",
+    "Kds"
+]
