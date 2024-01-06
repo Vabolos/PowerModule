48b309849ab9febca395c9f0f358f40e08cebc64
Vabolos <vaboloslol@gmail.com>
2024-01-06 23:08:06 +0100

    Refactored clear_console function in ClearConsoleButton.py

--- src/components/ClearConsoleButton.py
+++ src/components/ClearConsoleButton.py
@@ -2,4 +2,3 @@ def clear_console(self):
     self.textbox.configure(state="normal")
     self.textbox.delete("0.0", "end")
     self.textbox.configure(state="disabled")
-    print("Cleared console.")
