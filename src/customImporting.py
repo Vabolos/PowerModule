cb484092d7031315f5018894a31446dc90924ad0
Vabolos <vaboloslol@gmail.com>
2024-01-07 23:08:08 +0100

    Add customImporting module and update main.py

---
+++ src/customImporting.py
@@ -0,0 +1,19 @@
+# components
+from components.buttons.ClearConsoleButton import clear_console
+from components.buttons.ConsoleInputButton import open_input_dialog_event
+from components.buttons.CopyToBoardButton import copy_to_clipboard
+from components.buttons.executeButton import sidebar_button_event_scriptExe
+from components.buttons.discordButton import open_discord_server
+from components.buttons.githubButton import open_github_repository
+from components.buttons.openExplorerButton import open_explorer
+from components.buttons.internalNotepad import open_notepad
+from components.buttons.openPowershellButton import open_powershell
+
+from components.consoleEntry import AppFunctions
+
+# scripts
+from components.scriptcalls.ADM.getAdGroupMember import getAdGroupMember
+from components.scriptcalls.ADM.getGroupsMemberOf import getGroupsMemberOf
+from components.scriptcalls.ADM.exportCsv import exportCSV
+from components.scriptcalls.ADM.copyMemberOf import copyMemberOf
+from components.scriptcalls.ADM.getPasswordStatus import getPasswordStatus
