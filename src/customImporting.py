203ca57c91ea2bce8bc6a7843fa0143f66eb9f9b
Vabolos <vaboloslol@gmail.com>
2024-01-11 21:01:49 +0100

    Update import statements in customImporting.py

--- src/customImporting.py
+++ src/customImporting.py
@@ -2,13 +2,12 @@
 from components.buttons.Actions.ClearConsoleButton import clear_console
 from components.buttons.Actions.ConsoleInputButton import open_input_dialog_event
 from components.buttons.Actions.CopyToBoardButton import copy_to_clipboard
-from components.buttons.executeButton import sidebar_button_event_scriptExe
 from components.buttons.discordButton import open_discord_server
 from components.buttons.githubButton import open_github_repository
 from components.buttons.Actions.openExplorerButton import open_explorer
 from components.buttons.Actions.internalNotepad import open_notepad
 from components.buttons.Actions.openPowershellButton import open_powershell
-from components.buttons.importScript import import_script
+from components.buttons.importScript import *
 
 # components.misc
 from components.consoleEntry import AppFunctions
