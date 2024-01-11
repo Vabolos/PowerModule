7d07a47e4db534e0997f01c5bfb3f860af505a58
Vabolos <vaboloslol@gmail.com>
2024-01-11 21:01:42 +0100

    Remove unimplemented feature from executeButton.py

--- src/components/buttons/executeButton.py
+++
@@ -1,5 +0,0 @@
-import tkinter.messagebox
-
-def sidebar_button_event_scriptExe(self):
-    tkinter.messagebox.showinfo("Info", "This feature is not implemented yet.")
-    print("unimplemented feature was called")
