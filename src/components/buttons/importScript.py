3c72ef7e7efde8f4cf6a8615c05f767ec896d7cb
Vabolos <79908586+Vabolos@users.noreply.github.com>
2024-01-11 15:04:32 +0100

    Refactor import_script function and update sidebar buttons (still a WIP)

--- src/components/buttons/importScript.py
+++ src/components/buttons/importScript.py
@@ -1,4 +1,33 @@
-from components.buttons.Actions.openExplorerButton import open_explorer
+import filecmp
+import fileinput
+import subprocess
+import tkinter as tk
+from tkinter import messagebox
 
-def import_script():
-    open_explorer("Import Script", "Import", "Import PowerShell Script", "ps1")
+def import_script(self):
+    subprocess.Popen(r'explorer /select')
+    # only accept .ps1 files (only show these files in the file explorer)
+    if filecmp.endswith(".ps1"):
+        # open the file
+        with open(fileinput, "r") as f:
+            # read the file
+            data = f.read()
+            # insert the data into the console
+            self.consoleEntry.insert(tk.END, data)
+            # insert a new line
+            self.consoleEntry.insert(tk.END, "\n")
+            # focus the console
+            self.consoleEntry.focus()
+            # set the cursor to the end of the console
+            self.consoleEntry.see(tk.END)
+            # update the console
+            self.consoleEntry.update()
+    else:
+        # if the file is not a .ps1 file, show an error
+        messagebox.showerror("Error", "Only .ps1 files are supported!")
+        # focus the console
+        self.consoleEntry.focus()
+        # set the cursor to the end of the console
+        self.consoleEntry.see(tk.END)
+        # update the console
+        self.consoleEntry.update()
--- src/main.py
+++ src/main.py
@@ -36,11 +36,14 @@ class App(customtkinter.CTk):
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Power Module", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=lambda: open_github_repository(self), text="GitHub Repository")
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, 
+                                                        command=lambda: open_github_repository(self), text="GitHub Repository")
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=lambda: open_discord_server(self), text="Support Discord")
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, 
+                                                        command=lambda: open_discord_server(self), text="Support Discord")
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=lambda: clear_console(self), text="Import script")
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, 
+                                                        command=lambda: import_script(self), text="Import script")
         self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
