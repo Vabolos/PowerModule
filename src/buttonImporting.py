4b37f804de3fe6700dac17ac0bb42b4dbd8c1772
Vabolos <79908586+Vabolos@users.noreply.github.com>
2024-12-07 11:11:14 +0100

    feat: add buttonImporting.py to import button creation classes from the buttons directory

---
+++ src/buttonImporting.py
@@ -0,0 +1,4 @@
+# import classes from files in the buttons directory
+from buttons.ADM_buttons import create_adm_buttons
+from buttons.Machine_buttons import create_machine_buttons
+from buttons.Server_buttons import create_server_buttons
