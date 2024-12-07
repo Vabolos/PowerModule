b8debdad53acdda09cb854c445f4ea2132ecf347
Vabolos <79908586+Vabolos@users.noreply.github.com>
2024-12-07 10:34:19 +0100

    feat: add templateCall.py to quickly add new calls

---
+++ .templates/templateCall.py
@@ -0,0 +1,21 @@
+import subprocess
+
+def class_name_here(self):
+    # Specify the path to your PowerShell script
+    powershell_script = r'src/scripts/powermodules/ADM,Machine,Server/script_name_here.ps1'
+
+    try:
+        # Run the PowerShell script and pass the input as a parameter
+        scriptOutput = subprocess.check_output(['powershell', '-File', powershell_script], text=True, stderr=subprocess.STDOUT)
+
+        # Update the text in the custom Textbox with the PowerShell script output
+        self.textbox.configure(state= "normal")
+        self.textbox.delete("0.0", "end")
+        self.textbox.insert("0.0", scriptOutput)
+    except subprocess.CalledProcessError as e:
+        # Handle any errors that occurred during script execution
+        self.textbox.insert("0.0", f"Error: {e.output}")
+    finally:
+        # Update the state of the custom Textbox to disabled
+        self.textbox.configure(state="disabled")
+        
